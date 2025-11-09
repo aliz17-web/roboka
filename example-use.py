7ab48726491f82740318780937b4428118af83f7
aliz17-web <alizolg80@gmail.com>
2025-11-09 20:48:03 +0330

    Update and rename example-use.py to پیام همگانی.py

--- example-use.py
+++
@@ -1,8 +0,0 @@
-from roboka import Client
-# pip install -U roboka
-
-bot = Client("YOUR-TOKEN")
-
-for update in bot.on_message():
-	if update.text == "/start":
-		update.reply("hello from roboka.")
