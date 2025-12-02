14f7639a1ad9899a77c630bd7f6cd3d773c8cccd
aliz17-web <alizolg80@gmail.com>
2025-12-02 20:01:20 +0330

    Create markdown.py

---
+++ examples/markdown.py
@@ -0,0 +1,10 @@
+from roboka import Client, Update
+import asyncio
+
+bot = Client("token")
+
+@bot.on_message()
+async def hello(update: Update):
+    await update.reply("~~strike~~, **bold**, __italic__, ||spoiler||, ==underline==, `mono`, [link](example.com)")
+
+asyncio.run(bot.run())
