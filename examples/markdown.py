edee67489097117e074dcf786e9e9859f5df9c5a
aliz17-web <alizolg80@gmail.com>
2025-12-05 11:11:41 +0330

    Update markdown.py

--- examples/markdown.py
+++ examples/markdown.py
@@ -5,6 +5,6 @@ bot = Client("token")
 
 @bot.on_message()
 async def hello(update: Update):
-    await update.reply("~~strike~~, **bold**, __italic__, ||spoiler||, ==underline==, `mono`, [link](example.com)")
+    await update.reply("~~strike~~, **bold**, __italic__, ||spoiler||, ==underline==, `mono`, [link](example.com), ××quote××")
 
 asyncio.run(bot.run())
