1626d83241f7bfbb3e780f2dd21e3df01e42c117
aliz17-web <alizolg80@gmail.com>
2025-12-05 11:11:04 +0330

    Update html.py

--- examples/html.py
+++ examples/html.py
@@ -5,6 +5,6 @@ bot = Client("token")
 
 @bot.on_message()
 async def hello(update: Update):
-    await update.reply('<s>strike</s>, <b>bold</b>, <i>italic</i>, <span class="tg-spoiler">spoiler</span>, <u>underline</u>, <code>mono</code>, <a href="https://example.com">link</a>')
+    await update.reply('<s>strike</s>, <b>bold</b>, <i>italic</i>, <span class="tg-spoiler">spoiler</span>, <u>underline</u>, <code>mono</code>, <a href="https://example.com">link</a>, <q>quote</q>')
 
 asyncio.run(bot.run())
